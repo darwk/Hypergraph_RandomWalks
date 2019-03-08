767148c960ba1ef4c27e822d3dad19c6dc6ba905
darwk <darwinreddy.k@gmail.com>
2019-03-08 18:48:08 +0530

    Made changes "job.yaml" to "run.sh" files

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 # Note: all paths referenced here are relative to the Docker container.
 #
-# Add the Nvidia drivers to the path
-export PATH="/usr/local/nvidia/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/nvidia/lib:$LD_LIBRARY_PATH"
-# Tools config for CUDA, Anaconda installed in the common /tools directory
-source /tools/config.sh
+#source /tools/config.sh
 # Activate your environment
-source activate py35
+#source activate py35
+cd /storage/home/darwk/virtualenv
+source python3_env/bin/activate
+
 # Change to the directory in which your code is present
-cd /path/to/run.sh
+cd /storage/home/darwk/workspace/HypergraphDeepwalk/Hypergraph
 # Run the code. The -u option is used here to use unbuffered writes
 # so that output is piped to the file as and when it is produced.
-# Here, the code is the MNIST Tensorflow example.
-python __main__.py --num_walks 10  --walk_length 25 --num_dimensions 16 --window_size 10 --num_neighbors 10 &> out
+python -u __main__.py --num_walks 10  --walk_length 25 --num_dimensions 16 --window_size 10 --num_neighbors 10 &> out
